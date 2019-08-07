ccf89bb3cd8bee27e7fe95f9b49e366fc8c6d92c
Gustavo <gsinacio94@gmail.com>
2019-08-07 09:50:50 -0300

    adds basic time calculation when only date in is inserted

--- hours-calculation.py
+++ hours-calculation.py
@@ -4,11 +4,17 @@ import os
 from datetime import datetime
 from datetime import timedelta
 
+dateFormat = '%Y-%m-%dT%H:%M'
+printFormat = '%H:%M'
+
 def timeDeltaFormated(elapsed, delta):
   if elapsed > delta:
     return ('+', (elapsed - delta));
   else :
     return ('-', (delta - elapsed));
+
+def addTime(time, addminutes):
+  return (time + timedelta(minutes=addminutes))
 
 def addExtras(extras):
   time = timedelta(0)
@@ -20,7 +26,8 @@ def addExtras(extras):
 
   return time
 
-file = open('bank.txt', "r")
+filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bank.txt')
+file = open(filepath, "r")
 
 lines = file.readlines()
 
@@ -32,6 +39,14 @@ elapseds = []
 for line in lines:
 
   if len(line) < 33:
+    line = line.strip()
+
+    if len(line) == 16: # ------------------------------------------------------
+      time = datetime.strptime(line, dateFormat)
+      strTime = datetime.strftime(addTime(time, 510), printFormat)
+      print('Time in:  {}'.format(line.split('T')[1]))
+      print('Time out: {}'.format(strTime))
+    # --------------------------------------------------------------------------
     break
 
   startString = line[:-1].split(' ')[0]
@@ -39,8 +54,8 @@ for line in lines:
 
   delta = timedelta(hours=8, minutes=30)
   day = startString.split('T')[0]
-  start =  datetime.strptime(startString, '%Y-%m-%dT%H:%M')
-  end =  datetime.strptime(endString, '%Y-%m-%dT%H:%M')
+  start = datetime.strptime(startString, dateFormat)
+  end = datetime.strptime(endString, dateFormat)
   elapsed = (end - start)
 
   extraTime = timeDeltaFormated(elapsed, delta)
@@ -55,4 +70,4 @@ delta = addExtras(extras)
 
 formated = (delta.days*1440) + delta.seconds/60
 
-print("{:.0f} minutes".format(formated))
+print('General bank: {:.0f} minutes'.format(formated))
